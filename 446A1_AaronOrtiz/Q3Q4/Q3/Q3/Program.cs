1c8ced1b36bc12d085008c0609f14ad073e102f0
aaorti15 <116468231+aaorti15@users.noreply.github.com>
2026-01-19 13:56:37 -0700

    Add files via upload

---
+++ 446A1_AaronOrtiz/Q3Q4/Q3/Q3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Q3
+{
+    [ServiceContract]
+    public interface myInterface //Client side Interface
+    {
+        [OperationContract]
+        int SecretNumber(int lower, int upper);
+        [OperationContract]
+        string checkNumber(int userNum, int SecretNum);
+    }
+
+    public class myService : myInterface //use interface
+    {
+        public int SecretNumber(int lower, int upper) //Code to generate secrete number (given)
+        {
+            DateTime currentDate = DateTime.Now;
+            int seed = (int)currentDate.Ticks;
+            Random random = new Random(seed);
+            int sNumber = random.Next(lower, upper);
+            return sNumber;
+        }
+
+        public string checkNumber(int userNum, int SecretNum) //Code to check the number (given)
+        {
+            if (userNum == SecretNum)
+                return "correct";
+            else
+            if (userNum > SecretNum)
+                return "too big";
+            else return "too small";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8000/Service"); //Create Uri for local host usage
+
+            ServiceHost selfHost = new ServiceHost(typeof(myService), baseAddress); //Self hosting code
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(myInterface), new WSHttpBinding(), "myService"); //Apply the service to an endpoint for future use
+                System.ServiceModel.Description.ServiceMetadataBehavior smb = new System.ServiceModel.Description.ServiceMetadataBehavior { };
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb); //Add the metadata to the Host
+
+                selfHost.Open(); //Open Host
+                Console.WriteLine("myService is ready to take requests. Please create a clieant to call my service.");
+                Console.WriteLine("if you want to quit this service press enter");
+                Console.ReadLine();
+
+                selfHost.Close();//Close host
+            }
+            catch
+            {
+                Console.WriteLine("An exception occurred: (0)");
+                selfHost.Abort(); //Abort when error occurs
+            }
+        }
+    }
+}
---
+++ 446A1_AaronOrtiz/Q3Q4/Q3/Q4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myInterfaceClient myPxy = new myInterfaceClient(); //Create the proxy for the service
+
+            Console.Write("Enter Upper: ");     //Ask for upper limit of possible numbers
+            int upper = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Lower: ");     
+            int lower = Convert.ToInt32(Console.ReadLine()); //get user input for lower limit of numbers
+            int SecretNumber = myPxy.SecretNumber(lower, upper);
+            Console.WriteLine("A secret number has been generated"); //Indicate that number is generated
+            Console.Write("Enter a guess: ");
+            int userNum = Convert.ToInt32(Console.ReadLine()); //User inputted guess
+
+            while(userNum != SecretNumber) //Goes until user has entered correct number
+            {
+                Console.WriteLine(myPxy.checkNumber(userNum, SecretNumber)); //Uses checknumber service from proxy
+                Console.Write("Enter another guess: ");
+                userNum = Convert.ToInt32(Console.ReadLine()); //Changes UserNumber
+
+            }
+
+            Console.WriteLine(myPxy.checkNumber(userNum, SecretNumber)); //To get final string for completed
+            myPxy.Close(); //Close the proxy
+            Console.WriteLine("press enter to close the client."); 
+        }
+    }
+}
